12114caeec9c9765d69a38102c90d300355460b3
Nikita Pishel <pogerproger@gmail.com>
2024-12-01 16:47:58 +0000

    feat: load settings from JSON files

--- src/server/model/configuration.py
+++ src/server/model/configuration.py
@@ -1,34 +1,74 @@
 import math
+import json
+import os
+
+os.chdir('./src/server/data/presets')
 
 class Configuration:
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, filename):
         if cls._instance is None:
+
             cls._instance = super(Configuration, cls).__new__(cls)
+        
+            # Load standart settings
+            cls.loadConfig('standart.json')
 
-            cls.fieldSize = [32, 32]
-            cls.maxAgents = 20
-            
-            cls.maxGenomeLen = 5
-            cls.mutationChance = 0.1    # a chance of mutation of a new-born cell
-            cls.newNeuronChance = 0.3   # a chance in which cell adds new neuron instead of new connection
-
-            cls.maxActions = 1
-
+            # Load non-standart settings
+            cls.loadConfig(filename)
+        
+            # exceptional settings
             if cls.maxActions < 1:
                 raise Exception('configErr, maxActions below 1')
             
-            cls.geneTimeType = 'fixed'
-            cls.geneTimeValue = 100
+            if cls.geneTime < 1:
+                raise Exception('configErr, maxActions below 1')
 
-            if cls.geneTimeType == 'fixed':
-                cls.geneTime = cls.geneTimeValue
-            
-            elif cls.geneTimeType == 'length multiplier':
-                cls.geneTime = cls.geneTimeValue * cls.maxGenomeLen
-                math.floor(cls.geneTime)
-        
         return cls._instance
 
-config = Configuration()
+    @classmethod
+    def getInstance(cls, filename):
+        if Configuration._instance == None:
+            Configuration(filename)
+        return cls._instance
+
+    @classmethod
+    def loadConfig(cls, filename):
+        with open(filename, 'r') as file:
+            settings = json.load(file)
+
+        if settings['field size'] != None:
+            cls.fieldSize = settings['field size']
+
+        if settings['max amount of agents'] != None:
+            cls.maxAgents = settings['max amount of agents']
+        
+        if settings['max genome length'] != None:
+            cls.maxGenomeLen = settings['max genome length']
+
+        if settings['mutation chance'] != None:
+            cls.mutationChance = settings['mutation chance']
+
+        if settings['new neuron chance'] != None:
+            cls.newNeuronChance = settings['new neuron chance']
+
+        if settings['max actions'] != None:
+            cls.maxActions = settings['max actions']
+
+        geneRuntime = settings['gene runtime']
+        if geneRuntime != None:
+            multiplier = geneRuntime['multiplier']
+            dependecy = geneRuntime['dependency']
+
+            if dependecy == None:
+                cls.geneTime = multiplier * geneRuntime['fixed value']
+
+            elif dependecy == 'gene length':
+                cls.geneTime = multiplier * cls.maxGenomeLen
+                math.floor(cls.geneTime)
+            
+            else:
+                raise Exception('unknow dependency in \'geneRuntime\' variable')
+
+config = Configuration.getInstance('.example.json')
