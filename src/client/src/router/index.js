d4ea2c70c718374085869032d036db4a03c6bae8
Nikita Pishel <pogerproger@gmail.com>
2025-01-25 15:45:34 +0000

    feat: move status function, router changes

--- src/client/src/router/index.js
+++ src/client/src/router/index.js
@@ -8,22 +8,22 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/user/authorise',
-      name: 'sign-in',
-      component: userSignView,
-    },
-    {
-      path: '/user/read',
+      path: '/',
       name: 'read-data',
       component: readDataView,
       props: true
     },
     {
       path: '/user/write',
-      name: 'read-data',
-      component: readDataView,
+      name: 'ctrl-panel',
+      component: serverControlView,
       props: true
-    }
+    },
+    {
+      path: '/user/authorise',
+      name: 'sign-in',
+      component: userSignView,
+    },
   ]
 })
 
--- src/client/src/stores/simData.js
+++ src/client/src/stores/simData.js
@@ -3,23 +3,33 @@ import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 const STORE_NAME = 'simData'
-const CHANNEL_LIST_ENDPOINT = 'http://127.0.0.1'
+const BASE_URL = 'http://127.0.0.1:5000'
 
 export const useChannelsStore = defineStore(STORE_NAME, () => {
   const jsonData = ref({});
 
   const simStat = computed(() => jsonData.value.data);
+  
+  // fetch status from the server
+  async function getStatus() {
+      try {
+          const response = await axios.get(`${BASE_URL}/dev/status`);
+          console.log('Server Status:', response.data);
+      } catch (error) {
+          console.error('Error fetching status:', error);
+      }
+  }
 
-  async function loadData() {
+  async function loadStat() {
     try {
-      const response = await axios.get(CHANNEL_LIST_ENDPOINT)
+      const response = await axios.get(BASE_URL)
       jsonData.value = response.data
     } catch(e) {
       console.error('Error fetching the JSON file:', e);
     }
   };
   
-  loadData();
+  loadStat();
   
   return {
     simStat,
