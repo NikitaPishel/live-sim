d2a17e290cceed2a88f93a1963af4f5ff1657572
Nikita Pishel <pogerproger@gmail.com>
2025-01-26 14:31:41 +0000

    feat: add navigation panel

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
   import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="page">
     <div class="header">
-      <div class="navigation">Menu 1</div>
-      <div class="navigation">Menu 2</div>
+      <div class="navigation" id="read" @click="$router.push({ name: 'read-data', params: {} })"><a>Read</a></div>
+      <div class="navigation" id="write" @click="$router.push({ name: 'ctrl-panel', params: {} })"><a>Control</a></div>
+      <div class="navigation" id="theme" @click=""><img id="theme-icon" src="@/img/light-256.png"></div>
     </div>
     <div class="content">
       <RouterView />
@@ -18,8 +19,25 @@
 
 <style>
 .navigation {
-  margin-left: 0.3%;
-  margin-right: 0.3%;
+  padding: 0.5%;
+  padding-left: 1%;
+  padding-right: 1%;
+
+  margin-left: 0.2%;
+  margin-right: 0.2%;
+
+  border-style: solid;
+  border-bottom-style: none;
+  border-width: 2px;
+
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+
+  border-color: #3d3d3d;
+
+  background-color: #efe3da;
 }
 
 .page {
@@ -32,14 +50,13 @@
 
 .header {
   background-color: #d5c4b7;
-  
   border-color: #3d3d3d;
-  border-bottom-width: 5px;
+
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
   
   margin: 0;
-  padding: 12px;
-
-  height: 20%;
+  padding: 0;
 
   display: flex;
 }
@@ -48,7 +65,18 @@
   background-color: #efe3da;
   margin: 0%;
   padding: 0%;
+}
 
-  height: 80%;
+#read {
+
+}
+
+#control {
+
+}
+
+#theme-icon {
+  width: 24px;
+  height: 24px;
 }
 </style>
