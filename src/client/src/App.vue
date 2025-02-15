efbd586b9fd8004718955571216fa4d974421d2a
NikitaPishel <pogerproger@gmail.com>
2025-02-15 18:46:25 +0000

    feat: add theme and icon switch

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,15 +1,33 @@
 <script setup>
   import { ref, computed } from 'vue';
   import { RouterView } from 'vue-router';
+
+  const themeName = ref('dark')
+  const themeIcon = ref("/src/img/dark-256.png")
+
+
+  const switchTeme = () => {
+    console.log('Switching theme')
+
+    if (themeName.value === 'dark') {
+      themeName.value = 'light';
+      themeIcon.value = '/src/img/light-256.png';
+    }
+    
+    else {
+      themeName.value = 'dark';
+      themeIcon.value = '/src/img/dark-256.png';
+    };
+  };
 </script>
 
 <template>
-  <main class="dark">
+  <main :class="themeName">
     <div class="page">
       <div class="header">
         <div class="navigation" id="read" @click="$router.push({ name: 'read-data', params: {} })"><a>Read</a></div>
         <div class="navigation" id="write" @click="$router.push({ name: 'ctrl-panel', params: {} })"><a>Control</a></div>
-        <div class="navigation" id="theme" @click=""><img id="theme-icon" src="@/img/light-256.png"></div>
+        <div class="navigation" id="theme" @click="switchTeme"><img id="theme-icon" :src="themeIcon"></div>
         <div class="navigation" id="theme" @click="$router.push({ name: 'sign-in', params: {} })"><img id="theme-icon" src="@/img/light-256.png"></div>
       </div>
       <div class="pageContainer">
